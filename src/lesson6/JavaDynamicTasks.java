9c7f7c8d9b7be60a09e786dffc57132b35888370
Julia0028 <tsrk.j@mail.ru>
2019-11-30 15:58:51 +0300

    2 tasks from 6 lesson without tests

--- src/lesson6/JavaDynamicTasks.java
+++ src/lesson6/JavaDynamicTasks.java
@@ -18,7 +18,13 @@ public class JavaDynamicTasks {
      * Если есть несколько самых длинных общих подпоследовательностей, вернуть любую из них.
      * При сравнении подстрок, регистр символов *имеет* значение.
      */
-    //проверка на прохождение тестов котоеда, чуть позже добавлю оценку
+    //двумерный массив len[][] хранит длины подпоследовательностей
+    //сравниваем символы 2х строк: если равны - то элемент будет входить в подпоследовательность, добавляем +1 к длине
+    //иначе, если first[i] не входит в подпоследовательность, то нужно брать len[i-1][j], если second[i] - len[i][j - 1],
+    //среди них находим макс. В subSequence передаю заполненную матрицу длин, где наибольшая длина находится в правом нижнем углу
+    //начинаю идти от макс дллины, согласно нашим условиям, собирая элементы строк по индексу
+
+    //Сложность: O(n*m) Ресурсоемкость: O(n*m), где n - длина first, m - длина second
     public static String longestCommonSubSequence(String first, String second) {
         if (first.isEmpty() || second.isEmpty()) return "";
         StringBuilder res = new StringBuilder();
@@ -28,7 +34,7 @@ public class JavaDynamicTasks {
                 if (first.charAt(i - 1) != second.charAt(j - 1)) len[i][j] = Math.max(len[i - 1][j], len[i][j - 1]);
                 else len[i][j] = len[i - 1][j - 1] + 1;
             }
-        }
+        } //O(n*m)
         subSequence(len.length - 1, len[0].length - 1, len, first, second, res);
         return res.reverse().toString();
     }
@@ -58,13 +64,20 @@ public class JavaDynamicTasks {
      * то вернуть ту, в которой числа расположены раньше (приоритет имеют первые числа).
      * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
      */
-    //проверка на прохождение тестов котоеда, чуть позже добавлю оценку
+    //массив len будет хранить длины подпоследовательностей,
+    //сравнивая каждый элемент списка с предыдущим, нахожу длины возможных подпоследовательности
+    //затем нахожу максимальную длину подпоследовательности len[indLast],
+    // где indLast - индекс элемента, на кот закончилась подпоследовательность
+    //затем еще раз прохожу по списку до indLast, длины соседних чисел должны отличаться на 1, с этим условиюм формирую result
+    //while нужен, чтобы проходить каждый раз список сначала (для выполнения условия с возвратом числом, кот стоит раньше)
+
+    //Сложность: O(n^2) Ресурсоемкость: O(n), n - количество элементов списка
     public static List<Integer> longestIncreasingSubSequence(List<Integer> list) {
         List<Integer> res = new ArrayList<>();
         if (list.isEmpty()) return res;
         int[] len = new int[list.size()];
         int max = -1;
-        int indMax = 0;
+        int indLast = 0;
         for (int i = 1; i < list.size(); i++) {
             for (int j = 0; j < i; j++) {
                 if (list.get(i) > list.get(j) && len[j] > max) {
@@ -73,28 +86,28 @@ public class JavaDynamicTasks {
                 }
             }
             max = -1;
-        }
-    max = len[0];
+        } //O(n^2)
+    max = -1;
     for (int i = 0; i < list.size(); i++) {
         if (len[i]> max) {
             max = len[i];
-            indMax = i;
+            indLast = i;
         }
-    }
-    int a = indMax;
+    } //O(n)
+    res.add(list.get(indLast));
+    int a = indLast;
     int b = 0;
-    res.add(list.get(indMax));
     while (b != a) {
         for (int i = 0; i < a; i++) {
-            if (list.get(i) < list.get(indMax) && len[indMax] - len[i] == 1) {
+            if (list.get(i) < list.get(indLast) && len[indLast] - len[i] == 1) {
                 res.add(list.get(i));
-                indMax = i;
+                indLast = i;
                 break;
             }
         }
         b++;
-    }
-    Collections.reverse(res);
+    } //O(a^2), где 0 <= a <= n
+    Collections.reverse(res); //O(n)
     return res;
     }
 
